b9de80bc219ed2e77bda7b657ce4872d1abb4c0b
J <jeremiaxhlin@gmail.com>
2018-04-04 19:30:02 -0700

    REFACTORED: board data structure to fix square click event

--- src/index.js
+++ src/index.js
@@ -21,16 +21,16 @@ class Board extends React.Component {
         );
     }
 
-    renderRow(i){
+    renderRow(chunk_index, row_chunk){
 
         let row = [];
 
-        for(let i=0; i < this.props.length; i++){
-            row.push(this.renderSquare(i));
+        for(let i=0; i < row_chunk.length; i++){
+            row.push(this.renderSquare(row_chunk[i]));
         }
 
         return (
-            <div key={i} className="board-row">
+            <div key={chunk_index} className="board-row">
                 {row}
             </div>
         );
@@ -39,9 +39,15 @@ class Board extends React.Component {
     render() {
 
         let board = [];
+        const square_keys = this.props.squares.map((value, index) => {
+            return index;
+        });
+        const chunks = chunkArray(square_keys, this.props.length);
 
-        for(let i=0; i < this.props.length; i++){
-            board.push(this.renderRow(i));
+        for(let i=0; i < chunks.length; i++){
+
+            board.push(this.renderRow(i, chunks[i]));
+
         }
 
         return (
@@ -56,7 +62,7 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
 
-        this.square_length = 3;
+        this.square_length = 4;
         this.square_count = Math.pow(this.square_length, 2);
 
         this.state = {
@@ -68,6 +74,7 @@ class Game extends React.Component {
             stepNumber: 0,
             xIsNext: true
         };
+
     }
 
     handleClick(i) {
@@ -162,4 +169,24 @@ function calculateWinner(squares) {
         }
     }
     return null;
+}
+
+/**
+ * Splits an array into chunks given a size
+ *
+ * @param arr
+ * @param chunk_size
+ * @returns {Array}
+ */
+function chunkArray( arr, chunk_size){
+
+    let chunk;
+    let chunks = [];
+
+    for (let i = 0; i < arr.length; i += chunk_size) {
+        chunk = arr.slice(i, i + chunk_size);
+        chunks.push(chunk);
+    }
+
+    return chunks;
 }
