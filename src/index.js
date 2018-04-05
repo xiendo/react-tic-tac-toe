254defcd6a6d7004500eadc562064e72fe1ca45d
J <jeremiaxhlin@gmail.com>
2018-04-05 11:05:36 -0700

    FIXED: square handleClick event setting 'X' or 'O' properly in updated data structure

--- src/index.js
+++ src/index.js
@@ -81,10 +81,10 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? "X" : "O";
+        // if (calculateWinner(squares) || squares[i]) {
+        //     return;
+        // }
+        squares[i].player = this.state.xIsNext ? "X" : "O";
         this.setState({
             history: history.concat([
                 {
